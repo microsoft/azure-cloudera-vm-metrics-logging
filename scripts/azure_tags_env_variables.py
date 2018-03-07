a08431f214980286075bcccfa3db6268955e1657
Zach Miller <zmmille2@gmail.com>
2018-03-07 15:13:03 -0500

    added better file handling, comment, and log for debugging.

--- scripts/azure_tags_env_variables.py
+++ scripts/azure_tags_env_variables.py
@@ -1,3 +1,4 @@
+# allows us to run commands in a subprocess, and pipe the results of those processes into python at run time.
 from subprocess import Popen, PIPE
 import json
 
@@ -7,10 +8,9 @@ res = raw_response.stdout.read()
 res_json = json.loads(res)
 res_tags = res_json['compute']['tags']
 
-file_obj = open("AzureTagsEnvVariables", "w")
-for pair in res_tags.split(';'):
-  pair = pair.encode('ascii', 'ignore').upper()
-  key_value = pair.split(':')
-  file_obj.write("export AZ_{0}=\"{1}\"\n".format(key_value[0], key_value[1]))
-
-file_obj.close()
+with open("AzureTagsEnvVariables", "w") as file_obj:
+  for pair in res_tags.split(';'):
+    pair = pair.encode('ascii', 'ignore').upper()
+    key_value = pair.split(':')
+    print "key='{0}', value='{1}'\n".format(key_value[0], key_value[1])
+    file_obj.write("export AZ_{0}=\"{1}\"\n".format(key_value[0], key_value[1]))
